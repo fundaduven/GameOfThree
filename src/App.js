d96b238c90b2eba74efff43ad6697bd5f5a50118
Funda Duven <fundaduven@gmail.com>
2020-09-07 13:30:51 +0200

    Fix losing/winning status

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import socketIOClient from 'socket.io-client';
+import './App.css'
 
 const ENDPOINT = 'http://127.0.0.1:4001';
 const socket = socketIOClient(ENDPOINT);
@@ -56,27 +57,28 @@ class App extends Component {
      socket.emit('PLAYER_MOVED', {moveNumber: playedMove});
     }
 
+
   render() {
     let winnerTitle = null;
       if (this.state.winnerID === socket.id) {
-      winnerTitle = <h1> You win!</h1> } else if (!this.state.winnerID === socket.id){
-      winnerTitle = <h1>You lose</h1>
+      winnerTitle = <h1 className= 'animate__animated animate__zoomInDown animate__infinite animate__slow'> YOU ARE THE WINNER!!! </h1> } else if (this.state.isWinner && (this.state.winnerID !== socket.id)){
+      winnerTitle = <h1 className='animate__animated animate__zoomOutDown animate__infinite animate__slower dark-red'>Sorry... You lost. </h1>
      } else {
-      winnerTitle = <p> Who will be the winner? </p>
+      winnerTitle = <p className='baskerville'> Who will be the winner? </p>
    }
 
     return (
       <div>
-       <p>IT'S TIME TO {this.state.isStarted ? 'WIN' : 'WAIT'} </p>
+       <p className= 'bg-light-green avenir dib br4 b--dashed b--black-60 pa3 ma2 grow bw2 shadow-5 f3 b' id='p1'> IT'S TIME TO {this.state.isStarted ? 'PLAY' : 'WAIT'} </p>
         {this.state.isStarted ? 
         <div>
-         <p>game is starting with number: {this.state.number}</p> 
+         <p className= 'grow:hover animate__animated animate__tada animate__repeat-2' id='p2'> Game number is: {this.state.number}</p> 
           <button disabled={!this.state.canPlay || this.state.isWinner} id='-1' onClick={this.onButtonClicked}>-1</button>
           <button disabled={!this.state.canPlay || this.state.isWinner} id='0'  onClick={this.onButtonClicked}>0</button>
           <button disabled={!this.state.canPlay || this.state.isWinner} id='+1' onClick={this.onButtonClicked}>+1</button>
         </div>
         : 
-        <h1> waiting for other players</h1>}
+        <h1 className= 'i light-blue'> waiting for your playfellow :) </h1>}
 
         <h2>{winnerTitle}</h2>
       </div>
