896fb5750ffd07230fd48b589350dab4342f24b2
Funda Duven <fundaduven@gmail.com>
2020-09-10 17:32:23 +0200

    start new game feature

--- server/index.js
+++ server/index.js
@@ -1,61 +1,63 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const port = process.env.PORT || 4001;
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
 
 let NUMBER = Math.floor(Math.random() * 1000);
 const WINNING_NUMBER = 1;
 const PLAYERS = [];
 const isStarted = false;
 const turnID = 0;
-const winner = '';
-const isWinner=  false;
+const winner = "";
+const isWinner = false;
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   PLAYERS.push({
     id: socket.id,
-    socket 
-  })
-  console.log('New client connected', PLAYERS, PLAYERS.length);
+    socket,
+  });
+  console.log("New client connected", PLAYERS, PLAYERS.length);
 
-  if(PLAYERS.length === 2 ){
-    console.log('buraya geliyo mu?')
-   PLAYERS[0].socket.emit('GAME_CREATED', { number: NUMBER, isStarted: true, canPlay: true })
-   PLAYERS[1].socket.emit('GAME_CREATED', { number: NUMBER, isStarted: true, canPlay: false })
-  }
-  // ama bu sekilde yazinca kimin kazandigini tespit etmiyoruz aslinda?
-  // son hamleyi yapan 
-
-  
-  
-  socket.on('PLAYER_MOVED', (msg) => {
-    console.log('message: ' + msg.moveNumber +' //id burda'+ socket.id);
-    NUMBER = Math.floor((NUMBER + Number(msg.moveNumber))/3);
-    console.log('New number is', NUMBER)
-    const foundPlayer = PLAYERS.find(player => { 
-      return (player.id === socket.id)
+  if (PLAYERS.length === 2) {
+    console.log("buraya geliyo mu?");
+    PLAYERS[0].socket.emit("GAME_CREATED", {
+      number: NUMBER,
+      isStarted: true,
+      canPlay: true,
     });
-    const otherPlayer = PLAYERS.find(player => player.id !== foundPlayer.id );
-    
-    foundPlayer.socket.emit('GAME_UPDATED', { number: NUMBER, canPlay: false })
-    otherPlayer.socket.emit('GAME_UPDATED', { number: NUMBER, canPlay: true})
-    
-    if (NUMBER === WINNING_NUMBER){
-      console.log('oyun bitti mi?')
-      io.emit('GAME_FINISHED', { isWinner: true, winnerID: socket.id }) // oyun bitti state i, uida nasil gozuksun ona bak, 3. kisiyi eklemesin arraye, ayirmaya baslariz 
+    PLAYERS[1].socket.emit("GAME_CREATED", {
+      number: NUMBER,
+      isStarted: true,
+      canPlay: false,
+    });
+  }
+
+  socket.on("PLAYER_MOVED", (msg) => {
+    console.log("message: " + msg.moveNumber + " //id burda" + socket.id);
+    NUMBER = Math.floor((NUMBER + Number(msg.moveNumber)) / 3);
+    console.log("New number is", NUMBER);
+    const foundPlayer = PLAYERS.find((player) => {
+      return player.id === socket.id;
+    });
+    const otherPlayer = PLAYERS.find((player) => player.id !== foundPlayer.id);
+
+    foundPlayer.socket.emit("GAME_UPDATED", { number: NUMBER, canPlay: false });
+    otherPlayer.socket.emit("GAME_UPDATED", { number: NUMBER, canPlay: true });
+
+    if (NUMBER === WINNING_NUMBER) {
+      console.log("oyun bitti mi?");
+      io.emit("GAME_FINISHED", { isWinner: true, winnerID: socket.id }); // oyun bitti state i, uida nasil gozuksun ona bak, 3. kisiyi eklemesin arraye, ayirmaya baslariz
     }
   });
-  
-  
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+
+  socket.on("GAME_RESTART", () => {});
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
-
 server.listen(port, () => console.log(`Listening on port ${port}`));
-
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import socketIOClient from 'socket.io-client';
-import './App.css'
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import "./App.css";
 
-const ENDPOINT = 'http://127.0.0.1:4001';
+const ENDPOINT = "http://127.0.0.1:4001";
 const socket = socketIOClient(ENDPOINT);
 
 class App extends Component {
@@ -13,77 +13,134 @@ class App extends Component {
       isStarted: false,
       canPlay: false,
       isWinner: false,
-      winnerID: ''
+      winnerID: "",
     };
   }
 
-  
   componentDidMount() {
-    console.log('neredeyum')
-    socket.on('GAME_CREATED', (msg) => {
-      console.log('bisiler geldi', msg)
+    console.log("neredeyum");
+    socket.on("GAME_CREATED", (msg) => {
+      console.log("bisiler geldi", msg);
       this.setState(() => {
         return {
           number: msg.number,
           isStarted: msg.isStarted,
-          canPlay: msg.canPlay
+          canPlay: msg.canPlay,
         };
       });
-    })
-    socket.on('GAME_UPDATED', (msg) => {
-      console.log('bisiler degisti', msg)
+    });
+    socket.on("GAME_UPDATED", (msg) => {
+      console.log("bisiler degisti", msg);
       this.setState(() => {
         return {
           number: msg.number,
-          canPlay: msg.canPlay
+          canPlay: msg.canPlay,
         };
       });
-    })
+    });
 
-    socket.on('GAME_FINISHED', (msg) => {
-      console.log('oyun bitti', msg) 
+    socket.on("GAME_FINISHED", (msg) => {
+      console.log("oyun bitti", msg);
       this.setState(() => {
         return {
           isWinner: msg.isWinner,
-          winnerID: msg.winnerID
-        }
-      })
-    })
+          winnerID: msg.winnerID,
+        };
+      });
+    });
+  }
+
+  onButtonClicked = (event) => {
+    const playedMove = Number(event.target.id);
+    console.log("basildi", playedMove);
+    socket.emit("PLAYER_MOVED", { moveNumber: playedMove });
   };
 
-     onButtonClicked = (event) => {
-       const playedMove = Number(event.target.id)
-      console.log('basildi', playedMove) 
-     socket.emit('PLAYER_MOVED', {moveNumber: playedMove});
-    }
-
+  onNewGame = (event) => {
+    socket.emit("GAME_RESTART");
+  };
 
   render() {
-    let winnerTitle = null;
-      if (this.state.winnerID === socket.id) {
-      winnerTitle = <h1 className= 'animate__animated animate__zoomInDown animate__infinite animate__slow'> YOU ARE THE WINNER!!! </h1> } else if (this.state.isWinner && (this.state.winnerID !== socket.id)){
-      winnerTitle = <h1 className='animate__animated animate__zoomOutDown animate__infinite animate__slower dark-red'>Sorry... You lost. </h1>
-     } else {
-      winnerTitle = <p className='baskerville'> Who will be the winner? </p>
-   }
-
     return (
       <div>
-       <p className= 'bg-light-green avenir dib br4 b--dashed b--black-60 pa3 ma2 grow bw2 shadow-5 f3 b' id='p1'> IT'S TIME TO {this.state.isStarted ? 'PLAY' : 'WAIT'} </p>
-        {this.state.isStarted ? 
-        <div>
-         <p className= 'grow:hover animate__animated animate__tada animate__repeat-2' id='p2'> Game number is: {this.state.number}</p> 
-          <button disabled={!this.state.canPlay || this.state.isWinner} id='-1' onClick={this.onButtonClicked}>-1</button>
-          <button disabled={!this.state.canPlay || this.state.isWinner} id='0'  onClick={this.onButtonClicked}>0</button>
-          <button disabled={!this.state.canPlay || this.state.isWinner} id='+1' onClick={this.onButtonClicked}>+1</button>
-        </div>
-        : 
-        <h1 className= 'i light-blue'> waiting for your playfellow :) </h1>}
-
-        <h2>{winnerTitle}</h2>
+        <p
+          className="bg-light-green avenir dib br4 b--dashed b--black-60 pa3 ma2 grow bw2 shadow-5 f3 b"
+          id="p1"
+        >
+          IT'S TIME TO {this.state.isStarted ? "PLAY" : "WAIT"}
+        </p>
+        {this.state.isStarted ? (
+          <div>
+            <p
+              className="grow:hover animate__animated animate__tada animate__repeat-2"
+              id="p2"
+            >
+              Game number is: {this.state.number}
+            </p>
+            <button
+              disabled={!this.state.canPlay || this.state.isWinner}
+              id="-1"
+              onClick={this.onButtonClicked}
+            >
+              -1
+            </button>
+            <button
+              disabled={!this.state.canPlay || this.state.isWinner}
+              id="0"
+              onClick={this.onButtonClicked}
+            >
+              0
+            </button>
+            <button
+              disabled={!this.state.canPlay || this.state.isWinner}
+              id="+1"
+              onClick={this.onButtonClicked}
+            >
+              +1
+            </button>
+          </div>
+        ) : (
+          <h1 className="i light-blue"> waiting for your playfellow :) </h1>
+        )}
+        <WinnerTitle
+          winnerID={this.state.winnerID}
+          socketId={socket.id}
+          isWinner={this.state.isWinner}
+        />
+        <NewGame
+          isShowButton={this.state.isWinner}
+          onButtonClicked={this.onNewGame}
+        />
       </div>
     );
   }
 }
 
 export default App;
+
+////////
+function WinnerTitle({ winnerID, socketId, isWinner }) {
+  let winnerTitle;
+  if (winnerID === socketId) {
+    winnerTitle = (
+      <h1 className="animate__animated animate__zoomInDown animate__infinite animate__slow">
+        YOU ARE THE WINNER!!!
+      </h1>
+    );
+  } else if (isWinner && winnerID !== socketId) {
+    winnerTitle = (
+      <h1 className="animate__animated animate__zoomOutDown animate__infinite animate__slower dark-red">
+        Sorry... You lost.
+      </h1>
+    );
+  } else {
+    winnerTitle = <p className="baskerville"> Who will be the winner? </p>;
+  }
+  return winnerTitle;
+}
+
+function NewGame({ isShowButton, onButtonClicked }) {
+  return isShowButton ? (
+    <button onClick={onButtonClicked}>New Game</button>
+  ) : null;
+}
